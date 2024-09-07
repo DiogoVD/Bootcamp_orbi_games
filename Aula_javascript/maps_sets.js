73a77f56f375511ce83d7ba04e6f354fc1f66832
DiogoVD <diogovdamascena@gmail.com>
2024-09-07 12:46:16 -0300

    corrigindo

--- Aula_javascript/maps_sets.js
+++ Aula_javascript/maps_sets.js
@@ -35,8 +35,8 @@ console.log(getAdmins(usuarios))
 /*
 Atividade 2: Sets
 Dado o array [30, 30, 40, 5, 223, 2049, 3034, 5], retorne outro array apenas com valores únicos.
-- const meuArray = [30,30,40,5,223,2049,3034,5];
 */
+const meuArray = [30,30,40,5,223,2049,3034,5];
 
 function valoresUnicos(arr){
     const mySet = new Set(arr);
